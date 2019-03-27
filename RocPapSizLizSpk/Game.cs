eb8d31b8e26243230313be8bbe44ff574b93f41a
Ryan Kunz <ryanmkunz@gmail.com>
2019-03-27 12:04:21 -0500

    Laid out basic framework of classes

---
+++ RocPapSizLizSpk/Game.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+    class Game
+    {
+        //variables
+        string OpponentType;
+
+        public Game()
+        {
+
+        }
+        //methods
+        public void RunGame()
+        {
+            Human player1 = new Human();
+            Referee referee = new Referee();
+            do
+            {
+                Console.WriteLine("Would you like to play against the computer?");
+                OpponentType = Console.ReadLine();
+            } while (OpponentType != "yes" || OpponentType != "no");
+            if (OpponentType == "yes")
+            {
+                Computer player2 = new Computer();
+            }
+            else
+            {
+                Human player2 = new Human();
+            }
+                        
+        }
+    }
+}
